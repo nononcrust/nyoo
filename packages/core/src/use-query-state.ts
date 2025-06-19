d57a95ecbf70dee2db32aa5797edcd154bc88703
nonon <nononcrust@gmail.com>
2025-06-19 15:31:31 +0900

    history 옵션 추가

--- packages/core/src/use-query-state.ts
+++ packages/core/src/use-query-state.ts
@@ -3,6 +3,21 @@ import { objectEntries } from "./lib/object";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { compareValue, encodeValue } from "./lib/utils";
 
+const defaultOption: Required<UpdateOption> = {
+  history: "replace",
+} as const;
+
+type UpdateOption = {
+  /**
+   * 쿼리 파라미터가 업데이트될 때 브라우저 히스토리에 어떻게 반영될지를 설정합니다.
+   *
+   * `replace`: 현재 히스토리를 유지한 채 쿼리 파라미터 상태만 업데이트합니다. (기본값)
+   *
+   * `push`: 새로운 히스토리를 추가합니다. 이 경우, 뒤로 가기 버튼을 눌러 이전 상태로 돌아갈 수 있습니다.
+   */
+  history?: "replace" | "push";
+};
+
 /**
  * 쿼리 파라미터 상태를 관리하는 커스텀 훅입니다.
  *
@@ -21,21 +36,13 @@ export const useQueryState = <TSchema extends z.ZodType>(
 
   const value = schema.parse(Object.fromEntries(searchParams.entries()));
 
-  /**
-   * 하나의 쿼리 파라미터를 업데이트합니다.
-   *
-   * value로 전달한 값이 기본값과 동일하다면 해당 파라미터를 제거합니다.
-   *
-   * 동시에 여러 값을 업데이트해야 할 경우에는 `updateMany`를 사용하세요.
-   *
-   * @example
-   * update("page", 2);
-   * update("sort", "asc");
-   */
   const update = <TKey extends keyof TSearchParams>(
     key: TKey,
-    value: TSearchParams[TKey]
+    value: TSearchParams[TKey],
+    options?: UpdateOption
   ) => {
+    const optionsWithDefault = { ...defaultOption, ...options };
+
     const params = new URLSearchParams(searchParams.toString());
 
     if (compareValue(value, defaultValue[key])) {
@@ -44,22 +51,25 @@ export const useQueryState = <TSchema extends z.ZodType>(
       params.set(key.toString(), encodeValue(value));
     }
 
-    router.replace(pathname + "?" + params.toString());
+    const url = pathname + "?" + params.toString();
+
+    const updateAction: Record<Required<UpdateOption>["history"], () => void> =
+      {
+        push: () => router.push(url),
+        replace: () => router.replace(url),
+      };
+
+    updateAction[optionsWithDefault.history]();
   };
 
-  /**
-   * 여러 개의 쿼리 파라미터를 동시에 업데이트합니다.
-   *
-   * value로 전달한 값이 기본값과 동일하다면 해당 파라미터를 제거합니다.
-   *
-   * @example
-   * updateMany({ page: 2, sort: "asc" });
-   */
   const updateMany = (
     entries: Partial<
       Record<keyof TSearchParams, TSearchParams[keyof TSearchParams]>
-    >
+    >,
+    options?: UpdateOption
   ) => {
+    const optionsWithDefault = { ...defaultOption, ...options };
+
     const params = new URLSearchParams(searchParams.toString());
 
     objectEntries(entries).forEach(([key, value]) => {
@@ -70,18 +80,17 @@ export const useQueryState = <TSchema extends z.ZodType>(
       }
     });
 
-    router.replace(pathname + "?" + params.toString());
+    const url = pathname + "?" + params.toString();
+
+    const updateAction: Record<Required<UpdateOption>["history"], () => void> =
+      {
+        push: () => router.push(url),
+        replace: () => router.replace(url),
+      };
+
+    updateAction[optionsWithDefault.history]();
   };
 
-  /**
-   * 쿼리 파라미터를 문자열 형태로 반환합니다.
-   *
-   * @example
-   *
-   * getQueryString({ page: 2, sort: "asc" }) // "?page=2&sort=asc"
-   *
-   * router.replace('/products' + getQueryString({ page: 2, sort: "asc" }))
-   */
   const getQueryString = (
     entries: Partial<
       Record<keyof TSearchParams, TSearchParams[keyof TSearchParams]>
@@ -96,12 +105,6 @@ export const useQueryState = <TSchema extends z.ZodType>(
     return "?" + params.toString();
   };
 
-  /**
-   * 특정 쿼리 파라미터를 제거합니다.
-   *
-   * @example
-   * remove("page");
-   */
   const remove = (key: keyof TSearchParams) => {
     const params = new URLSearchParams(searchParams.toString());
 
@@ -110,23 +113,71 @@ export const useQueryState = <TSchema extends z.ZodType>(
     router.replace(pathname + "?" + params.toString());
   };
 
-  /**
-   * 모든 쿼리 파라미터를 제거하고 기본값으로 초기화합니다.
-   *
-   * @example
-   * clear();
-   */
   const clear = () => {
     router.replace(pathname);
   };
 
   return {
+    /**
+     * 현재 쿼리 파라미터 상태를 나타내는 값입니다.
+     *
+     * @example
+     * value.page // 1
+     * value.sort // "asc"
+     */
     value,
+    /**
+     * 커스텀 훅을 정의할 때 전달한 기본값입니다.
+     *
+     * @example
+     * defaultValue.page // 1
+     * defaultValue.sort // "asc"
+     */
     defaultValue,
+    /**
+     * 쿼리 파라미터를 문자열 형태로 반환합니다.
+     *
+     * @example
+     *
+     * getQueryString({ page: 2, sort: "asc" }) // "?page=2&sort=asc"
+     *
+     * router.replace('/products' + getQueryString({ page: 2, sort: "asc" }))
+     */
     getQueryString,
+    /**
+     * 하나의 쿼리 파라미터를 업데이트합니다.
+     *
+     * value로 전달한 값이 기본값과 동일하다면 해당 파라미터를 제거합니다.
+     *
+     * 동시에 여러 값을 업데이트해야 할 경우에는 `updateMany`를 사용하세요.
+     *
+     * @example
+     * update("page", 2);
+     * update("sort", "asc");
+     */
     update,
+    /**
+     * 여러 개의 쿼리 파라미터를 동시에 업데이트합니다.
+     *
+     * value로 전달한 값이 기본값과 동일하다면 해당 파라미터를 제거합니다.
+     *
+     * @example
+     * updateMany({ page: 2, sort: "asc" });
+     */
     updateMany,
+    /**
+     * 특정 쿼리 파라미터를 제거합니다.
+     *
+     * @example
+     * remove("page");
+     */
     remove,
+    /**
+     * 모든 쿼리 파라미터를 제거하고 기본값으로 초기화합니다.
+     *
+     * @example
+     * clear();
+     */
     clear,
   };
 };
